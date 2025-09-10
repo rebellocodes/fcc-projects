4eece7f532a2e66ad618e8c41d044a736ad78938
Victor Rebello <joshurebello2794@gmail.com>
2025-09-10 20:26:23 +0100

    js trivia bot

---
+++ JS/Trivia Bot/script.js
@@ -0,0 +1,19 @@
+console.log("Hello, I'm your coding fun fact guide!");
+
+let botName = "Victor";
+let botLocation = "England";
+let favoriteLanguage = "Javascript";
+
+console.log("My name is " + botName + " and i live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " is a language that adds behavior to web pages" + ".";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is an object oriented language" + ".";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is the most widely used programming language in the world" + ".";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
