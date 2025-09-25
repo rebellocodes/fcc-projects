9841c6bb25ead7dcb740902b92d7f82c2cd5590a
Victor Rebello <joshurebello2794@gmail.com>
2025-09-25 21:01:12 +0100

    add quiz game project

---
+++ JS/Quiz Game/script.js
@@ -0,0 +1,32 @@
+const questions = [
+    {
+        category: "Geography",
+        question: "Which continent is India Located in?",
+        choices:["Asia","Europe","North America"],
+        answer: "Asia"
+    },
+    {
+        category: "Health",
+        question: "What is the full form of BMI?",
+        choices:["Black Men India","Body Mass Index","Body Mass Integer"],
+        answer: "Body Mass Index"
+    },
+    {
+        category: "Sports",
+        question: "What is CR7?",
+        choices:["Cantwell Rudy","Chip Rodriogues","Cristiano Ronaldo"],
+        answer: "Cristiano Ronaldo"
+    },
+    {
+        category: "Food",
+        question: "Which fast food place has the golden arches?",
+        choices:["Subway","Dominos","McDonalds"],
+        answer: "McDonalds"
+    },
+    {
+        category: "Cars",
+        question: "Which is a German Car company?",
+        choices:["Mercedes","Nissan","Mcclaren"],
+        answer: "Mercedes"
+    }
+];
