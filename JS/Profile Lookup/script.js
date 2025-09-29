82e14bf77450353bb32570a868230ab855ac3d86
Victor Rebello <joshurebello2794@gmail.com>
2025-09-29 21:33:41 +0100

    profile lookup

---
+++ JS/Profile Lookup/script.js
@@ -0,0 +1,38 @@
+let contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+const lookUpProfile = (name,property) => {
+    for(const info of contacts){
+        if (info.firstName === name){
+            if(info[property] !== undefined){
+                return info[property]
+            } else {
+                return "No such property"
+            }
+        } 
+    }
+    return "No such contact"
+}
